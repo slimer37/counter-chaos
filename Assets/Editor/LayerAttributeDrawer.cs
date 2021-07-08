6773b07161f9a155c0519f58ea1507ccf53a46b5
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-07-07 19:51:38 -0700

    Create layer attribute drawer for single-layer fields

---
+++ Assets/Editor/LayerAttributeDrawer.cs
@@ -0,0 +1,17 @@
+using Core;
+using UnityEngine;
+using UnityEditor;
+
+namespace Project.Editor
+{
+    [CustomPropertyDrawer(typeof(LayerAttribute))]
+    class LayerAttributeEditor : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            property.intValue = EditorGUI.LayerField(position, label, property.intValue);
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Assets/Scripts/Core/CustomAttributes.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+
+namespace Core
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class LayerAttribute : PropertyAttribute { }
+}
