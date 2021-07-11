ac8ecafd14e230cdc5faad62623632edac4ea35b
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-07-10 21:41:12 -0700

    Non-interactables now correctly block Interaction raycasts

--- Assets/Scripts/Interactables/Interaction.cs
+++ Assets/Scripts/Interactables/Interaction.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using Core;
 
 namespace Interactables
 {
     public class Interaction : MonoBehaviour
     {
         [SerializeField] float reach;
-        [SerializeField] LayerMask mask;
+        [SerializeField, Layer] int interactablesLayer;
         [SerializeField] new Camera camera;
         [SerializeField] IconHandler iconHandler;
 
@@ -15,15 +16,25 @@ namespace Interactables
 
         void Update()
         {
-            if (Physics.Raycast(camera.ViewportPointToRay(new Vector3(0.5f, 0.5f)), out var hit, reach, mask))
+            if (Physics.Raycast(camera.ViewportPointToRay(new Vector3(0.5f, 0.5f)), out var hit, reach))
             {
+                if (hit.transform.gameObject.layer != interactablesLayer)
+                {
+                    HoverOff();
+                    return;
+                }
+                
                 var hoverable = hit.transform.GetComponent<Hoverable>();
                 
                 if (hoverable)
                     (hoveredTransform = hoverable).OnHover(iconHandler);
             }
-            else if (hoveredTransform)
+            else
+                HoverOff();
+
+            void HoverOff()
             {
+                if (!hoveredTransform) return;
                 hoveredTransform.OnHoverExit();
                 hoveredTransform = null;
             }
