8e76bfce331d9c27b464b8549333ceae87c637e1
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-07-17 15:16:48 -0700

    Fix namespace and add barcode and id

--- Assets/Scripts/Products/ProductInfo.cs
+++ Assets/Scripts/Products/ProductInfo.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Game/Product", fileName = "New Product")]
-public class ProductInfo : ScriptableObject
+namespace Products
 {
-    [field: SerializeField] public string DisplayName { get; private set; }
-    [field: SerializeField] public float Price { get; private set; }
-    [field: SerializeField, TextArea] public string Description { get; private set; }
+    [CreateAssetMenu(menuName = "Game/Product", fileName = "New Product")]
+    public class ProductInfo : ScriptableObject
+    {
+        [field: SerializeField] public string DisplayName { get; private set; }
+        [field: SerializeField] public float Price { get; private set; }
+        [field: SerializeField, TextArea] public string Description { get; private set; }
+    
+        public int ID { get; private set; }
+        public Texture2D Barcode { get; private set; }
+
+        public void Init(int id)
+        {
+            ID = id;
+            Barcode = Products.Barcode.Generate(id);
+        }
+    }
 }
