e2b911a26c9cbc26a09376ca3a7153821e2da3cd
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-07-18 16:07:25 -0700

    Init() creates unique 5-digit ID

--- Assets/Scripts/Products/ProductInfo.cs
+++ Assets/Scripts/Products/ProductInfo.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Products
 {
@@ -12,10 +15,22 @@ namespace Products
         public int ID { get; private set; }
         public Texture2D Barcode { get; private set; }
 
-        public void Init(int id)
+        static List<int> usedIDs = new List<int>();
+        const int IDLength = 5;
+
+        public void Init(int barcodeSeed)
         {
-            ID = id;
-            Barcode = Products.Barcode.Generate(id);
+            string idString;
+            do
+            {
+                idString = Random.Range(1, 10).ToString();
+                for (var i = 0; i < IDLength - 1; i++)
+                    idString += Random.Range(0, 10);
+            } while (usedIDs.Contains(Convert.ToInt32(idString)));
+            
+            ID = Convert.ToInt32(idString);
+            usedIDs.Add(ID);
+            Barcode = Products.Barcode.Generate(barcodeSeed);
         }
     }
 }
