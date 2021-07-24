5980450241f8a0a57395245da262d9652c6cbe57
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-07-24 01:13:45 -0700

    Update BarcodeDisplay.cs

--- Assets/Scripts/Products/BarcodeDisplay.cs
+++ Assets/Scripts/Products/BarcodeDisplay.cs
@@ -1,25 +1,27 @@
 using DG.Tweening;
-using Products;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
-public class BarcodeDisplay : MonoBehaviour
+namespace Products
 {
-    [SerializeField] CanvasGroup uiGroup;
-    [SerializeField] RawImage image;
-    [SerializeField] TextMeshProUGUI id;
-    [SerializeField] float fadeTime;
-
-    void Awake() => uiGroup.alpha = 0;
-
-    public void ShowBarcodeFor(ProductInfo info)
+    public class BarcodeDisplay : MonoBehaviour
     {
-        uiGroup.DOComplete();
-        uiGroup.DOFade(1, fadeTime);
-        image.texture = info.Barcode;
-        id.text = info.ID.ToString();
-    }
+        [SerializeField] CanvasGroup uiGroup;
+        [SerializeField] RawImage image;
+        [SerializeField] TextMeshProUGUI id;
+        [SerializeField] float fadeTime;
 
-    public void HideBarcode() => uiGroup.DOFade(0, fadeTime);
+        void Awake() => uiGroup.alpha = 0;
+
+        public void ShowBarcodeFor(ProductInfo info)
+        {
+            uiGroup.DOComplete();
+            uiGroup.DOFade(1, fadeTime);
+            image.texture = info.Barcode;
+            id.text = info.ID.ToString();
+        }
+
+        public void HideBarcode() => uiGroup.DOFade(0, fadeTime);
+    }
 }
