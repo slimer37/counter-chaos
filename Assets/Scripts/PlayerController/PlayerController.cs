e6c9d4f8535234857d036fcb089ee67e92ea1285
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-08-02 15:23:23 -0700

    Remove PlayerController namespace

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -2,165 +2,162 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace PlayerController
+public class PlayerController : MonoBehaviour
 {
-    public class PlayerController : MonoBehaviour
+    [SerializeField] CharacterController characterController;
+    
+    [Header("Movement")]
+    [SerializeField] float walkSpeed;
+    [SerializeField] float sprintSpeed;
+    [SerializeField] float transitionTime;
+    [SerializeField] float sprintFov;
+    [SerializeField] Ease transitionEase;
+    
+    [Header("Jumping")]
+    [SerializeField] float jumpForce;
+    [SerializeField] float maxJumpTime;
+    [SerializeField, Min(0)] float gravity = 9.81f;
+    
+    [Header("Looking")]
+    [SerializeField] Camera cam;
+    [SerializeField] Camera secondaryCam;
+    [SerializeField] Transform body;
+    [SerializeField] float sensitivity;
+    [SerializeField] float rotLimit;
+
+    [Header("Bobbing")]
+    [SerializeField] float bobAmount;
+    [SerializeField] float bobTime;
+    [SerializeField] float bobResetSpeed;
+    [SerializeField] Ease bobEase;
+
+    float originalCamY;
+    Tween bobTween;
+    Tween returnTween;
+
+    Sequence sprintTransition;
+    bool isSprinting;
+    float currentSpeed;
+
+    float jumpTime;
+    bool isHoldingJump;
+    
+    Vector2 mouseDelta;
+    Vector3 camRot;
+    Vector3 moveVector;
+
+    bool canMove = true;
+    bool canLook = true;
+
+    void OnDisable() => bobTween?.Kill();
+    void OnEnable() => UpdateBobbing();
+
+    void EnableMovement(bool value) => canMove = value;
+    void EnableLook(bool value) => canLook = value;
+
+    void OnMove(InputValue val)
     {
-        [SerializeField] CharacterController characterController;
+        if (!enabled) return;
         
-        [Header("Movement")]
-        [SerializeField] float walkSpeed;
-        [SerializeField] float sprintSpeed;
-        [SerializeField] float transitionTime;
-        [SerializeField] float sprintFov;
-        [SerializeField] Ease transitionEase;
+        var moveInput = val.Get<Vector2>();
+        moveVector = new Vector3(moveInput.x, moveVector.y, moveInput.y);
         
-        [Header("Jumping")]
-        [SerializeField] float jumpForce;
-        [SerializeField] float maxJumpTime;
-        [SerializeField, Min(0)] float gravity = 9.81f;
+        UpdateBobbing();
+    }
+
+    void UpdateBobbing()
+    {
+        var wasBobbing = bobTween != null && bobTween.active && bobTween.IsPlaying();
+        var isBobbing = moveVector.x != 0 || moveVector.z != 0;
+
+        if (wasBobbing == isBobbing) return;
         
-        [Header("Looking")]
-        [SerializeField] Camera cam;
-        [SerializeField] Camera secondaryCam;
-        [SerializeField] Transform body;
-        [SerializeField] float sensitivity;
-        [SerializeField] float rotLimit;
-
-        [Header("Bobbing")]
-        [SerializeField] float bobAmount;
-        [SerializeField] float bobTime;
-        [SerializeField] float bobResetSpeed;
-        [SerializeField] Ease bobEase;
-
-        float originalCamY;
-        Tween bobTween;
-        Tween returnTween;
-
-        Sequence sprintTransition;
-        bool isSprinting;
-        float currentSpeed;
-
-        float jumpTime;
-        bool isHoldingJump;
+        bobTween?.Kill();
+        returnTween?.Kill();
         
-        Vector2 mouseDelta;
-        Vector3 camRot;
-        Vector3 moveVector;
+        var camT = cam.transform;
+        var resetDuration = (originalCamY - camT.localPosition.y) / bobResetSpeed;
+        
+        returnTween = camT.DOLocalMoveY(originalCamY, resetDuration);
+        
+        if (!isBobbing) return;
+        bobTween = cam.transform.DOLocalMoveY(originalCamY - bobAmount, bobTime)
+            .SetDelay(resetDuration).SetLoops(-1, LoopType.Yoyo).SetEase(bobEase);
+    }
 
-        bool canMove = true;
-        bool canLook = true;
-
-        void OnDisable() => bobTween?.Kill();
-        void OnEnable() => UpdateBobbing();
-
-        void EnableMovement(bool value) => canMove = value;
-        void EnableLook(bool value) => canLook = value;
-
-        void OnMove(InputValue val)
+    void OnJump(InputValue val)
+    {
+        if (val.isPressed && characterController.isGrounded)
         {
-            if (!enabled) return;
-            
-            var moveInput = val.Get<Vector2>();
-            moveVector = new Vector3(moveInput.x, moveVector.y, moveInput.y);
-            
-            UpdateBobbing();
+            isHoldingJump = true;
+            moveVector.y = jumpForce;
+            jumpTime = maxJumpTime;
         }
+        else if (!val.isPressed)
+            isHoldingJump = false;
+    }
 
-        void UpdateBobbing()
+    void OnSprint(InputValue val) => isSprinting = val.isPressed;
+    void OnMoveMouse(InputValue val) => mouseDelta = sensitivity * val.Get<Vector2>();
+
+    void Awake()
+    {
+        camRot = cam.transform.localEulerAngles;
+        originalCamY = cam.transform.localPosition.y;
+        currentSpeed = walkSpeed;
+        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        // Construct sprint transition.
+        sprintTransition = DOTween.Sequence();
+        sprintTransition.Append(DOTween.To(() => currentSpeed,
+            newSpeed => currentSpeed = newSpeed, sprintSpeed, transitionTime));
+        sprintTransition.Join(cam.DOFieldOfView(sprintFov, transitionTime));
+        sprintTransition.Join(secondaryCam.DOFieldOfView(sprintFov, transitionTime));
+        sprintTransition.SetEase(transitionEase).SetAutoKill(false).Pause();
+    }
+
+    void OnDestroy() => sprintTransition.Kill();
+
+    void Update()
+    {
+        // Jumping & Gravity
+
+        if (isHoldingJump && jumpTime > 0)
+            jumpTime -= Time.deltaTime;
+        else if (!characterController.isGrounded)
+            moveVector.y -= gravity * Time.deltaTime;
+        
+        // Moving
+
+        if (canMove)
+            characterController.Move(currentSpeed * Time.deltaTime * body.TransformDirection(moveVector));
+        
+        // Looking
+        
+        if (canLook)
         {
-            var wasBobbing = bobTween != null && bobTween.active && bobTween.IsPlaying();
-            var isBobbing = moveVector.x != 0 || moveVector.z != 0;
-
-            if (wasBobbing == isBobbing) return;
-            
-            bobTween?.Kill();
-            returnTween?.Kill();
-            
-            var camT = cam.transform;
-            var resetDuration = (originalCamY - camT.localPosition.y) / bobResetSpeed;
-            
-            returnTween = camT.DOLocalMoveY(originalCamY, resetDuration);
-            
-            if (!isBobbing) return;
-            bobTween = cam.transform.DOLocalMoveY(originalCamY - bobAmount, bobTime)
-                .SetDelay(resetDuration).SetLoops(-1, LoopType.Yoyo).SetEase(bobEase);
+            camRot.x = Mathf.Clamp(camRot.x - mouseDelta.y * Time.deltaTime, -rotLimit, rotLimit);
+            body.localEulerAngles += mouseDelta.x * Time.deltaTime * Vector3.up;
+            cam.transform.localEulerAngles = camRot;
         }
-
-        void OnJump(InputValue val)
+        
+        // Sprinting
+        
+        if (canMove)
         {
-            if (val.isPressed && characterController.isGrounded)
+            if (moveVector.z > 0)
             {
-                isHoldingJump = true;
-                moveVector.y = jumpForce;
-                jumpTime = maxJumpTime;
-            }
-            else if (!val.isPressed)
-                isHoldingJump = false;
-        }
+                if (sprintTransition.IsPlaying() && isSprinting) return;
 
-        void OnSprint(InputValue val) => isSprinting = val.isPressed;
-        void OnMoveMouse(InputValue val) => mouseDelta = sensitivity * val.Get<Vector2>();
-
-        void Awake()
-        {
-            camRot = cam.transform.localEulerAngles;
-            originalCamY = cam.transform.localPosition.y;
-            currentSpeed = walkSpeed;
-            
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
-            
-            // Construct sprint transition.
-            sprintTransition = DOTween.Sequence();
-            sprintTransition.Append(DOTween.To(() => currentSpeed,
-                newSpeed => currentSpeed = newSpeed, sprintSpeed, transitionTime));
-            sprintTransition.Join(cam.DOFieldOfView(sprintFov, transitionTime));
-            sprintTransition.Join(secondaryCam.DOFieldOfView(sprintFov, transitionTime));
-            sprintTransition.SetEase(transitionEase).SetAutoKill(false).Pause();
-        }
-
-        void OnDestroy() => sprintTransition.Kill();
-
-        void Update()
-        {
-            // Jumping & Gravity
-
-            if (isHoldingJump && jumpTime > 0)
-                jumpTime -= Time.deltaTime;
-            else if (!characterController.isGrounded)
-                moveVector.y -= gravity * Time.deltaTime;
-            
-            // Moving
-
-            if (canMove)
-                characterController.Move(currentSpeed * Time.deltaTime * body.TransformDirection(moveVector));
-            
-            // Looking
-            
-            if (canLook)
-            {
-                camRot.x = Mathf.Clamp(camRot.x - mouseDelta.y * Time.deltaTime, -rotLimit, rotLimit);
-                body.localEulerAngles += mouseDelta.x * Time.deltaTime * Vector3.up;
-                cam.transform.localEulerAngles = camRot;
-            }
-            
-            // Sprinting
-            
-            if (canMove)
-            {
-                if (moveVector.z > 0)
-                {
-                    if (sprintTransition.IsPlaying() && isSprinting) return;
-
-                    if (isSprinting)
-                        sprintTransition.PlayForward();
-                    else
-                        sprintTransition.PlayBackwards();
-                }
-                else if (!sprintTransition.isBackwards)
+                if (isSprinting)
+                    sprintTransition.PlayForward();
+                else
                     sprintTransition.PlayBackwards();
             }
+            else if (!sprintTransition.isBackwards)
+                sprintTransition.PlayBackwards();
         }
     }
 }
