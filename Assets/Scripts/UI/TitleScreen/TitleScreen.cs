433e87acd7bbbcae395a03e3464853b08d766b06
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-08-08 20:10:10 -0700

    Add button to go to prototype scene

---
+++ Assets/Scripts/UI/TitleScreen/TitleScreen.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.TitleScreen
+{
+    public class TitleScreen : MonoBehaviour
+    {
+        public void LoadScene(int index) => SceneManager.LoadScene(index);
+    }
+}
