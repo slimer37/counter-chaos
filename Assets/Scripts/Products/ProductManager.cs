2b62fe327011e48162cb7ea0c34fd7b9641c1497
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-08-08 20:24:56 -0700

    Reduce ProductManager initialization and checks

--- Assets/Scripts/Products/ProductManager.cs
+++ Assets/Scripts/Products/ProductManager.cs
@@ -8,25 +8,11 @@ namespace Products
         [SerializeField] ProductInfo[] allProducts;
 
         static List<ProductIdentifier> productInstances;
-        static ProductInfo[] currentProductCollection;
         
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        static void Init()
-        {
-            if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 0) return;
-            productInstances = new List<ProductIdentifier>();
-            currentProductCollection = FindObjectOfType<ProductManager>().allProducts;
-        }
+        static void Init() => productInstances = new List<ProductIdentifier>();
 
-        public static void RegisterProduct(ProductIdentifier productIdentifier)
-        {
-#if UNITY_EDITOR
-            var info = productIdentifier.productInfo;
-            if (!System.Linq.Enumerable.Contains(currentProductCollection, info))
-                Debug.LogWarning($"{info.name} ({info.DisplayName}) is not in the product manager.");
-#endif
-            productInstances.Add(productIdentifier);
-        }
+        public static void RegisterProduct(ProductIdentifier productIdentifier) => productInstances.Add(productIdentifier);
         public static void DeregisterProduct(ProductIdentifier productIdentifier) => productInstances.Remove(productIdentifier);
 
         public static ProductIdentifier GetRandomProductInstance() => productInstances[Random.Range(0, productInstances.Count)];
