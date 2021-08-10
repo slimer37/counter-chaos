31a40ff8f2de4d09a262bfc23f25eedf2cda1538
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-08-09 18:30:06 -0700

    Replace TitleScreen.cs with ButtonDestination script

---
+++ Assets/Scripts/UI/ButtonDestination.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonDestination : MonoBehaviour
+    {
+        [SerializeField] int sceneIndex;
+        Button button;
+        void Awake() => button = GetComponent<Button>();
+        void OnEnable() => button.onClick.AddListener(LoadScene);
+        void OnDisable() => button.onClick.RemoveListener(LoadScene);
+        void LoadScene() => SceneLoader.Load(sceneIndex);
+    }
+}
--- Assets/Scripts/UI/TitleScreen/TitleScreen.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace UI.TitleScreen
-{
-    public class TitleScreen : MonoBehaviour
-    {
-        public void LoadScene(int index) => SceneLoader.Load(index);
-    }
-}
