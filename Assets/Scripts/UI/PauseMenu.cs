c6d5a216e770348ef211eb243f5076097da2efe8
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-08-09 18:33:47 -0700

    Dispose controls instance when pause menu is destroyed

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -2,44 +2,49 @@ using Core;
 using DG.Tweening;
 using UnityEngine;
 
-public class PauseMenu : MonoBehaviour
+namespace UI
 {
-    [SerializeField] CanvasGroup fadeGroup;
-    [SerializeField] float fadeTime;
-    
-    Controls controls;
-
-    bool paused;
-
-    CursorLockMode tempLockMode;
-    bool tempCursorVisibility;
-    
-    void Awake()
+    public class PauseMenu : MonoBehaviour
     {
-        controls = new Controls();
-        controls.Menu.Exit.performed += _ => TogglePause();
-        controls.Enable();
-        
-        fadeGroup.gameObject.SetActive(true);
-        fadeGroup.alpha = 0;
-        fadeGroup.blocksRaycasts = false;
-    }
+        [SerializeField] CanvasGroup fadeGroup;
+        [SerializeField] float fadeTime;
+    
+        Controls controls;
 
-    public void TogglePause()
-    {
-        paused = !paused;
-        Time.timeScale = paused ? 0 : 1;
-        fadeGroup.blocksRaycasts = paused;
-        fadeGroup.DOComplete();
-        fadeGroup.DOFade(paused ? 1 : 0, fadeTime).SetUpdate(true);
+        bool paused;
 
-        if (paused)
+        CursorLockMode tempLockMode;
+        bool tempCursorVisibility;
+    
+        void Awake()
         {
-            tempLockMode = Cursor.lockState;
-            tempCursorVisibility = Cursor.visible;
-        }
+            controls = new Controls();
+            controls.Menu.Exit.performed += _ => TogglePause();
+            controls.Enable();
         
-        Cursor.lockState = paused ? CursorLockMode.None : tempLockMode;
-        Cursor.visible = paused || tempCursorVisibility;
+            fadeGroup.gameObject.SetActive(true);
+            fadeGroup.alpha = 0;
+            fadeGroup.blocksRaycasts = false;
+        }
+
+        void OnDestroy() => controls.Dispose();
+
+        public void TogglePause()
+        {
+            paused = !paused;
+            Time.timeScale = paused ? 0 : 1;
+            fadeGroup.blocksRaycasts = paused;
+            fadeGroup.DOComplete();
+            fadeGroup.DOFade(paused ? 1 : 0, fadeTime).SetUpdate(true);
+
+            if (paused)
+            {
+                tempLockMode = Cursor.lockState;
+                tempCursorVisibility = Cursor.visible;
+            }
+        
+            Cursor.lockState = paused ? CursorLockMode.None : tempLockMode;
+            Cursor.visible = paused || tempCursorVisibility;
+        }
     }
 }
