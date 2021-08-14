4a1c39c28aee8a49ff291a5024c3e588fd0fcc16
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-08-14 12:22:59 -0700

    Update Controls.cs

--- Assets/Scripts/Core/Controls.cs
+++ Assets/Scripts/Core/Controls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Settings/Controls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Resources/Controls.inputactions'
 
 using System;
 using System.Collections;
