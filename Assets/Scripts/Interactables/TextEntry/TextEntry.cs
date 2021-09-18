815edaa0c600b99f36adc4758db5e7c4dca7c64a
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-09-18 11:16:58 -0700

    Switch to IMGUI text entry (temporary until UI update branch merge)

--- Assets/Scripts/Interactables/TextEntry/TextEntry.cs
+++ Assets/Scripts/Interactables/TextEntry/TextEntry.cs
@@ -1,57 +1,84 @@
 using Interactables.Base;
 using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace Interactables
 {
     public class TextEntry : MonoBehaviour, IInteractHandler
     {
-        [SerializeField] Hoverable hoverable;
         [SerializeField] TextMeshPro text;
         [SerializeField, Min(1)] int charLimit = 1;
 
         bool enteringText;
+        bool initGui;
         PlayerController tempController;
 
-        void InputID(char c)
+        void OnGUI()
         {
-            if (!enteringText || text.text.Length >= charLimit || char.IsControl(c)) return;
-            text.text += c;
-        }
+            if (!enteringText) return;
 
-        void Update()
-        {
-            if (!enteringText || text.text.Length == 0) return;
+            var size = 500;
 
-            var keyboard = Keyboard.current;
+            var center = new Rect(size / 2, Screen.height / 2, Screen.width - size, 500);
+            
+            GUILayout.BeginArea(center);
+            GUILayout.BeginVertical();
 
-            if (keyboard.backspaceKey.wasPressedThisFrame)
-                text.text = text.text.Remove(text.text.Length - 1);
-
-            if (keyboard.enterKey.wasPressedThisFrame)
+            var style = new GUIStyle(GUI.skin.box)
             {
-                enteringText = false;
-                tempController.Suspend(false);
-            }
-        }
+                alignment = TextAnchor.MiddleCenter,
+                fontSize = 50
+            };
+            var buttonStyle = new GUIStyle(GUI.skin.button) {fontSize = 50};
+            
+            GUI.SetNextControlName("text");
+            text.text = GUILayout.TextArea(text.text, style, GUILayout.Height(100));
+            GUI.FocusControl("text");
 
-        void OnEnable()
-        {
-            Keyboard.current.onTextInput += InputID;
-            hoverable.enabled = true;
-        }
-    
-        void OnDisable()
-        {
-            Keyboard.current.onTextInput -= InputID;
-            hoverable.enabled = false;
+            if (!initGui)
+            {
+                initGui = true;
+                var editor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
+                editor.selectIndex = text.text.Length;
+            }
+
+            if (text.text.Length > charLimit)
+                text.text = text.text.Substring(0, charLimit);
+            
+            GUILayout.Space(10);
+            
+            GUILayout.BeginHorizontal();
+
+            var difference = 400;
+            
+            GUILayout.Space(difference / 2);
+            
+            if (GUILayout.Button("Exit", buttonStyle, GUILayout.Width(center.width - difference), GUILayout.Height(50)))
+                EndEntry();
+            
+            GUILayout.EndHorizontal();
+            
+            GUILayout.EndVertical();
+            GUILayout.EndArea();
         }
 
         public void OnInteract(Transform sender)
         {
+            if (enteringText) return;
             enteringText = true;
             (tempController = sender.GetComponent<PlayerController>()).Suspend();
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            initGui = false;
+        }
+
+        void EndEntry()
+        {
+            if (!enteringText) return;
+            enteringText = false;
+            tempController.Suspend(false);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
         }
     }
 }
