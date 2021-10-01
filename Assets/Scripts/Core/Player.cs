48f8197dda286791b0c5fa9b28640a04b2c871ec
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-09-30 20:57:53 -0700

    Add Transform property of static Player (from root of Camera)

--- Assets/Scripts/Core/Player.cs
+++ Assets/Scripts/Core/Player.cs
@@ -6,7 +6,10 @@ namespace Core
     public static class Player
     {
         public static Camera Camera => camera ??= Camera.main;
+        public static Transform Transform => transform ??= Camera.transform.root;
+        
         static Camera camera;
+        static Transform transform;
 
         [RuntimeInitializeOnLoadMethod]
         static void Attach() => SceneManager.sceneLoaded += (s, l) => camera = null;
