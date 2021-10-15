bef467d4ccbed929bb59c1da843354199a603d07
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-10-15 12:35:48 -0700

    Add ClearPrefs button for editor

---
+++ Assets/Editor/ClearPrefs.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Project.Editor
+{
+    public static class ClearPrefs
+    {
+        [MenuItem("Tools/Clear Prefs")]
+        static void Clear()
+        {
+            PlayerPrefs.DeleteAll();
+            Debug.Log("Deleted preferences.");
+        }
+    }
+}
