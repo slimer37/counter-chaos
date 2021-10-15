bf4d0d19b113de95c6969e82cbca80e3a0b72467
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-10-15 12:29:42 -0700

    Add tutorial prompt

---
+++ Assets/Scripts/Tutorial/TutorialPrompt.cs
@@ -0,0 +1,40 @@
+﻿using Core;
+using UI;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Tutorial
+{
+    public class TutorialPrompt : MonoBehaviour
+    {
+        [SerializeField] string promptTitle;
+        [SerializeField, TextArea] string promptMessage;
+        [SerializeField] Color promptColor;
+        [SerializeField] int tutorialSceneIndex;
+        [SerializeField] int defaultSceneIndex;
+        
+        static bool PlayerDidTutorial => PlayerPrefs.GetInt(Key, 0) == 1;
+
+        const string Key = "TutorialDone";
+
+        // Also use this for manual tutorial access (i.e. from settings)
+        public void DoTutorial() => SceneLoader.Load(tutorialSceneIndex);
+        void DeclineTutorial() => SceneLoader.Load(defaultSceneIndex);
+
+        public void BeginPrompt()
+        {
+            if (!PlayerDidTutorial)
+            {
+                Dialog.Instance.YesNo(promptTitle, promptMessage, a => {
+                    if (a) DoTutorial();
+                    else DeclineTutorial();
+                }, promptColor);
+            }
+            else
+                DeclineTutorial();
+            
+            PlayerPrefs.SetInt(Key, 1);
+            PlayerPrefs.Save();
+        }
+    }
+}
