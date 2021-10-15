3fbaac90cf98828fd305f55ff8a90e14b5c3ba5a
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-10-15 00:19:37 -0700

    Make cam pan in title screen

---
+++ Assets/Scripts/UI/TitleScreen/CameraPan.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace UI.TitleScreen
+{
+    public class CameraPan : MonoBehaviour
+    {
+        [SerializeField] Transform end;
+        [SerializeField] Ease ease;
+        [SerializeField] float duration;
+
+        Sequence pan;
+
+        void Start()
+        {
+            pan = DOTween.Sequence();
+            pan.Append(transform.DOMove(end.position, duration).SetEase(ease));
+            pan.Join(transform.DORotate(end.eulerAngles, duration).SetEase(ease));
+            pan.SetLoops(-1, LoopType.Yoyo);
+        }
+    }
+}
