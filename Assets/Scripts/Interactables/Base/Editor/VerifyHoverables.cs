5c52742a2fe41c51dd08550e3dbbfa4ae7331a47
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-10-14 23:38:44 -0700

    Add editor class to check if all interact handlers have hoverable components attached

---
+++ Assets/Scripts/Interactables/Base/Editor/VerifyHoverables.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Interactables.Base.Editor
+{
+    public static class VerifyHoverables
+    {
+        [InitializeOnLoadMethod]
+        static void CheckForHoverables()
+        {
+            foreach (var go in Object.FindObjectsOfType<GameObject>())
+                if (go.TryGetComponent<IInteractHandler>(out _) && !go.TryGetComponent<Hoverable>(out _))
+                    Debug.LogError($"{go.name} is missing a Hoverable component.");
+        }
+    }
+}
