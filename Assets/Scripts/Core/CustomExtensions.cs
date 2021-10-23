0ad1d603615be869559c4f8797b51c5fea6a3349
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-10-22 20:55:13 -0700

    Create extension method to generate formatted string for binds

---
+++ Assets/Scripts/Core/CustomExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.InputSystem;
+
+namespace Core
+{
+    public static class CustomExtensions
+    {
+        public static string FormatDisplayString(this InputBinding binding, bool withBrackets = true)
+        {
+            var bind = binding.ToDisplayString(InputBinding.DisplayStringOptions.DontIncludeInteractions).ToUpper();
+            return withBrackets ? $"[{bind}]" : bind;
+        }
+    }
+}
