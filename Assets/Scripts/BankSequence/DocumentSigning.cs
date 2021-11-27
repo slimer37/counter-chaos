0e9ba235e79523bf1798e85527c51c46dce25dd6
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-11-27 14:26:36 -0800

    Document raises when the player looks in its direction

--- Assets/Scripts/BankSequence/DocumentSigning.cs
+++ Assets/Scripts/BankSequence/DocumentSigning.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,18 +8,35 @@ namespace BankSequence
 {
     public class DocumentSigning : MonoBehaviour
     {
-        [SerializeField] Chair chair;
+        [SerializeField] Transform cam;
+        [SerializeField] Vector3 expectedFacingDirection;
+        [SerializeField, Range(-1, 1)] float dotProductMinimum;
         [SerializeField] TMP_Text documentText;
+        [SerializeField] Transform upPosition;
+        [SerializeField] float focusDuration;
+        [SerializeField] Ease ease;
 
         int nameIndex;
         
         string signature = "";
         string originalText;
 
+        bool ableToSign;
+
         Keyboard keyboard;
-        
+
+        Sequence upSequence;
+
+        void OnDrawGizmosSelected()
+        {
+            if (!cam || expectedFacingDirection.sqrMagnitude == 0) return;
+            Gizmos.DrawRay(cam.position, expectedFacingDirection.normalized);
+        }
+
         void Awake()
         {
+            expectedFacingDirection.Normalize();
+            
             originalText = documentText.text = documentText.text.Replace("{0}", DateTime.Now.ToLongDateString());
             nameIndex = originalText.IndexOf("{1}", StringComparison.Ordinal);
             originalText = originalText.Replace("{1}", "");
@@ -26,11 +44,19 @@ namespace BankSequence
             
             keyboard = Keyboard.current;
             keyboard.onTextInput += OnTextInput;
+
+            upSequence = DOTween.Sequence();
+            upSequence.Append(transform.DOMove(upPosition.position, focusDuration).SetEase(ease));
+            upSequence.Join(transform.DORotate(upPosition.eulerAngles, focusDuration).SetEase(ease));
+            upSequence.Pause().SetAutoKill(false);
         }
 
         void Update()
         {
-            if (signature.Length == 0) return;
+            ableToSign = Vector3.Dot(cam.forward, expectedFacingDirection) > dotProductMinimum;
+            Focus(ableToSign);
+            
+            if (!ableToSign || signature.Length == 0) return;
             if (keyboard.backspaceKey.wasPressedThisFrame)
             {
                 signature = signature.Remove(signature.Length - 1);
@@ -40,11 +66,19 @@ namespace BankSequence
         
         void OnTextInput(char c)
         {
-            if (char.IsControl(c)) return;
+            if (!ableToSign || char.IsControl(c)) return;
             signature += c;
             UpdateText();
         }
 
         void UpdateText() => documentText.text = originalText.Insert(nameIndex, signature);
+
+        void Focus(bool up)
+        {
+            if (up != upSequence.isBackwards) return;
+            
+            if (up) upSequence.PlayForward();
+            else upSequence.PlayBackwards();
+        }
     }
 }
