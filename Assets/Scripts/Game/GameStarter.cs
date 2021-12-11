ce1a3d9ef6a072f5bcfb5cfd8c853997fddb0a0d
slimer37 <59073967+slimer37@users.noreply.github.com>
2021-12-10 23:56:17 -0800

    Replace TutorialPrompt with GameStarter with separate new game and load game scenes
    (both go to prototype for now)

--- Assets/Scripts/Game/GameStarter.cs
+++ Assets/Scripts/Game/GameStarter.cs
@@ -1,18 +1,22 @@
-﻿using Core;
+using Core;
 using UI;
 using UnityEngine;
-using UnityEngine.Events;
 
-namespace Tutorial
+namespace Game
 {
-    public class TutorialPrompt : MonoBehaviour
+    public class GameStarter : MonoBehaviour
     {
+        [SerializeField] int tutorialSceneIndex;
+        [SerializeField] int defaultSceneIndex;
+        [SerializeField] int newGameSceneIndex;
+        
+        [Header("Tutorial Prompt")]
         [SerializeField] string promptTitle;
         [SerializeField, TextArea] string promptMessage;
         [SerializeField, TextArea] string promptNewVersionMessage;
         [SerializeField] Color promptColor;
-        [SerializeField] int tutorialSceneIndex;
-        [SerializeField] int defaultSceneIndex;
+        
+        int destination;
         
         static bool PlayerDidLatestTutorial => PlayerPrefs.GetString(Key) == Application.version;
         static bool PlayerDidOldTutorial => PlayerPrefs.HasKey(Key) && PlayerPrefs.GetString(Key) != Application.version;
@@ -21,18 +25,30 @@ namespace Tutorial
 
         // Also use this for manual tutorial access (i.e. from settings)
         public void DoTutorial() => SceneLoader.Load(tutorialSceneIndex);
-        void DeclineTutorial() => SceneLoader.Load(defaultSceneIndex);
+        void DeclineTutorial() => SceneLoader.Load(destination);
+        
+        public void StartNewGame()
+        {
+            destination = newGameSceneIndex;
+            BeginPrompt();
+        }
+        
+        public void ContinueGame()
+        {
+            destination = defaultSceneIndex;
+            BeginPrompt();
+        }
 
-        public void BeginPrompt()
+        void BeginPrompt()
         {
             if (!PlayerDidLatestTutorial)
             {
                 Dialog.Instance.YesNo(promptTitle,
                     PlayerDidOldTutorial ? promptNewVersionMessage : promptMessage, 
                     a => {
-                    if (a) DoTutorial();
-                    else DeclineTutorial();
-                }, promptColor);
+                        if (a) DoTutorial();
+                        else DeclineTutorial();
+                    }, promptColor);
             }
             else
                 DeclineTutorial();
