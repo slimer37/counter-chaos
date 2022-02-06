2b540a90f873fb236d637d8e669e55ac7aff42d2
slimer37 <59073967+slimer37@users.noreply.github.com>
2022-02-05 22:23:58 -0800

    Improve thumbnail stuff

--- Assets/Scripts/Preview/Thumbnail.cs
+++ Assets/Scripts/Preview/Thumbnail.cs
@@ -5,7 +5,7 @@ namespace Preview
 {
     public static class Thumbnail
     {
-        public static readonly Vector2Int Dimensions = Vector2Int.one * 128;
+        static readonly Vector2Int DefaultDimensions = Vector2Int.one * 128;
         const bool Orthographic = true;
         
         static readonly Dictionary<string, Texture2D> Thumbnails = new();
@@ -27,23 +27,36 @@ namespace Preview
             light.transform.localPosition = Vector3.back * 0.5f;
             RuntimePreviewGenerator.InternalLight = light;
         }
+        
+        /// <summary>
+        /// Overload for Grab() that uses the default size. See
+        /// <see cref="Grab(string,UnityEngine.Transform,UnityEngine.Vector2Int)"/>
+        /// </summary>
+        public static Texture2D Grab(string key, Transform obj) => Grab(key, obj, DefaultDimensions);
 
-        public static Texture2D Grab(string key, Transform obj)
+        /// <summary>
+        /// Creates a new object thumbnail.
+        /// </summary>
+        /// <param name="key">If null or empty, the cache will not be used.
+        /// Otherwise, if a thumbnail has already been created with this key, it will be returned.</param>
+        /// <param name="obj">The subject of the thumbnail.</param>
+        /// <param name="size">The thumbnail's dimensions.</param>
+        /// <returns>The generated or cached thumbnail.</returns>
+        public static Texture2D Grab(string key, Transform obj, Vector2Int size)
         {
-            var model = obj.transform;
+            var dontUseCache = string.IsNullOrEmpty(key);
             
-            if (string.IsNullOrEmpty(key))
-            {
-                Debug.LogWarning("Key was null or empty. Using transform name.");
-                key = model.name;
-            }
+            if (!dontUseCache && Thumbnails.ContainsKey(key)) return Thumbnails[key];
 
-            if (Thumbnails.ContainsKey(key)) return Thumbnails[key];
-
-            var icon = RuntimePreviewGenerator.GenerateModelPreview(model, Dimensions.x, Dimensions.y);
+            var icon = RuntimePreviewGenerator.GenerateModelPreview(obj, size.x, size.y);
             icon.Compress(true);
-            icon.name = key;
-            return Thumbnails[key] = icon;
+            icon.filterMode = FilterMode.Bilinear;
+            
+            icon.name = string.IsNullOrEmpty(key) ? "Unnamed" : key;
+
+            if (!dontUseCache) Thumbnails[key] = icon;
+            
+            return icon;
         }
     }
 }
