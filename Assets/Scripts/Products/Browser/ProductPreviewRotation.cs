5e1442c8184d460c4619fe99a7809a522f3f4043
slimer37 <59073967+slimer37@users.noreply.github.com>
2022-02-09 21:06:43 -0800

    Rotate product image while idle

--- Assets/Scripts/Products/Browser/ProductPreviewRotation.cs
+++ Assets/Scripts/Products/Browser/ProductPreviewRotation.cs
@@ -3,14 +3,50 @@ using UnityEngine.EventSystems;
 
 namespace Products.Browser
 {
-    public class ProductPreviewRotation : MonoBehaviour, IDragHandler
+    public class ProductPreviewRotation : MonoBehaviour, IDragHandler, IPointerDownHandler, IEndDragHandler
     {
         [SerializeField] float sensitivity = 1;
+        [SerializeField] float idleSpeed = 1;
+        [SerializeField] float speedUpTime = 1;
+        [SerializeField] float idleTime = 1;
         [SerializeField] Transform rotator;
-        
-        public void OnDrag(PointerEventData eventData)
+
+        bool freeze;
+        bool idle;
+
+        float timeIdling;
+        float t;
+
+        void OnEnable() => idle = true;
+
+        void Update()
         {
-            rotator.Rotate(Vector3.up * eventData.delta.x * sensitivity);
+            if (freeze) return;
+            
+            if (!idle)
+            {
+                timeIdling += Time.deltaTime;
+                
+                if (timeIdling > idleTime)
+                {
+                    idle = true;
+                    t = 0;
+                }
+                
+                return;
+            }
+            
+            rotator.Rotate(Vector3.up * Mathf.Lerp(0, idleSpeed, t) * Time.deltaTime);
+            t += Time.deltaTime / speedUpTime;
+        }
+
+        public void OnPointerDown(PointerEventData eventData) => freeze = true;
+        public void OnDrag(PointerEventData eventData) => rotator.Rotate(Vector3.up * eventData.delta.x * sensitivity);
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            freeze = false;
+            idle = false;
+            timeIdling = 0;
         }
     }
 }
