57a0a7f8a4aa8e52d7c036067c0db8d587e6e18c
slimer37 <59073967+slimer37@users.noreply.github.com>
2022-02-19 23:57:20 -0800

    Truck leaves when items vacate trailer

--- Assets/Scripts/OrderSystem/DeliveryTruck.cs
+++ Assets/Scripts/OrderSystem/DeliveryTruck.cs
@@ -10,17 +10,32 @@ namespace OrderSystem
         [SerializeField] Transform end;
         [SerializeField] Transform itemSpawn;
         [SerializeField] float speed;
+        
+        [SerializeField] Vector3 trailerCenter;
+        [SerializeField] Vector3 trailerSize;
 
         Sequence deliverSequence;
+        
         Shipment currentShipment;
+
+        bool isStopped;
+
+        void OnDrawGizmosSelected()
+        {
+            if (itemSpawn)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.matrix = transform.localToWorldMatrix;
+                Gizmos.DrawWireCube(trailerCenter, trailerSize);
+            }
+        }
 
         void Awake()
         {
             deliverSequence = DOTween.Sequence();
             deliverSequence.AppendCallback(() => transform.SetPositionAndRotation(start.position, start.rotation));
             deliverSequence.Append(transform.DOMove(stop.position, speed).SetSpeedBased().SetEase(Ease.OutSine));
-            deliverSequence.AppendCallback(() => FillTruckTrailer(currentShipment));
-            deliverSequence.Append(transform.DOMove(end.position, speed).SetSpeedBased().SetEase(Ease.InSine));
+            deliverSequence.AppendCallback(() => StopAndFillTrailer(currentShipment));
             deliverSequence.Pause().SetAutoKill(false);
         }
 
@@ -35,13 +50,34 @@ namespace OrderSystem
             deliverSequence.Restart();
         }
 
-        void FillTruckTrailer(Shipment shipment)
+        void FixedUpdate()
         {
+            if (!isStopped) return;
+
+            if (CheckEmpty())
+                Leave();
+        }
+
+        void StopAndFillTrailer(Shipment shipment)
+        {
+            isStopped = true;
             foreach (var item in shipment.items)
             {
-                var clone = item.product.Instantiate();
-                clone.transform.position = itemSpawn.position;
+                for (var i = 0; i < item.quantity; i++)
+                {
+                    var clone = item.product.Instantiate();
+                    clone.transform.position = itemSpawn.position;
+                }
             }
         }
+
+        void Leave()
+        {
+            isStopped = false;
+            transform.DOMove(end.position, speed).SetSpeedBased().SetEase(Ease.InSine);
+        }
+
+        bool CheckEmpty() =>
+            !Physics.CheckBox(transform.TransformPoint(trailerCenter), trailerSize / 2, transform.rotation);
     }
 }
