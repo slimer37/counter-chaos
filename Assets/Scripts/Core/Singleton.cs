b8b2f248ab44eed9485d448e222bfb2486376574
slimer37 <59073967+slimer37@users.noreply.github.com>
2022-05-08 19:56:04 -0700

    Simplified singleton and added selfstartsingleton class

--- Assets/Scripts/Core/Singleton.cs
+++ Assets/Scripts/Core/Singleton.cs
@@ -3,37 +3,39 @@ using UnityEngine;
 
 namespace Core
 {
+    public abstract class SelfStartSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        static T instance;
+        
+        public static T Instance
+        {
+            get
+            {
+                if (instance) return instance;
+
+                instance = new GameObject(nameof(T)).AddComponent<T>();
+                DontDestroyOnLoad(instance.gameObject);
+                
+                return instance;
+            }
+        }
+    }
+
     public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
-        static bool shuttingDown;
-        static object lockObj = new();
         static T instance;
 
         public static T Instance
         {
             get
             {
-                if (shuttingDown)
-                {
-                    Debug.LogWarning("Singleton " + typeof(T) + " already destroyed. Returning null.");
-                    return null;
-                }
+                if (instance) return instance;
+                
+                instance = FindObjectOfType<T>();
+                if (!instance) throw new Exception("Couldn't find any " + typeof(T) + " objects.");
 
-                lock (lockObj)
-                {
-                    if (!instance)
-                    {
-                        instance = FindObjectOfType<T>();
-					
-                        if (!instance)
-                            throw new Exception("Couldn't find any " + typeof(T) + " objects.");
-                    }
-
-                    return instance;
-                }
+                return instance;
             }
         }
-
-        void OnApplicationQuit() => shuttingDown = true;
     }
 }
