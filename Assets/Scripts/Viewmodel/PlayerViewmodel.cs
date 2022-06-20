a19f96008eb0bd45c55ef7d351134520a6ed4828
Alfred <59073967+slimer37@users.noreply.github.com>
2022-06-19 22:13:43 -0700

    Add item dropping viewmodel

---
+++ Assets/Scripts/Viewmodel/PlayerViewmodel.cs
@@ -0,0 +1,46 @@
+using System;
+using Interactables.Holding;
+using UnityEngine;
+
+namespace Viewmodel
+{
+    public class PlayerViewmodel : MonoBehaviour
+    {
+        [Header("Rig")]
+        [SerializeField] Transform target;
+        [SerializeField] float offCenterMultiplier = 0.5f;
+        [SerializeField] Transform rootBone;
+        [SerializeField] float armLength;
+        
+        [Header("Misc")]
+        [SerializeField] Renderer rend;
+        [SerializeField] ItemHolder holder;
+        [SerializeField] GameObject ghost;
+
+        Vector3 originalLocalPos;
+
+        void Awake()
+        {
+            originalLocalPos = transform.InverseTransformPoint(rootBone.position);
+        }
+
+        void Update()
+        {
+            rend.enabled = holder.IsDroppingItem && !ghost.activeSelf;
+            
+            if (!rend.enabled) return;
+
+            var item = holder.HeldItem;
+            target.position =
+                item.transform.position
+                + Vector3.up * item.ToTopDistance
+                - item.transform.right * (item.BoundHalfDiagonal * offCenterMultiplier);
+
+            var originalPos = transform.TransformPoint(originalLocalPos);
+            var outward = target.position - originalPos;
+            rootBone.position = outward.sqrMagnitude > armLength * armLength
+                ? target.position - outward.normalized * armLength
+                : originalPos;
+        }
+    }
+}
