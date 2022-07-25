fa1d31799a89f47b319167e2d424e1deaf6332cc
Alfred <59073967+slimer37@users.noreply.github.com>
2022-07-25 10:50:19 +0800

    Add Addressable function to product info editor

--- Assets/Scripts/Products/Editor/ProductInfoEditor.cs
+++ Assets/Scripts/Products/Editor/ProductInfoEditor.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
 using UnityEngine;
 
 namespace Products.Editor
@@ -6,6 +8,14 @@ namespace Products.Editor
     [CustomEditor(typeof(ProductInfo))]
     public class ProductInfoEditor : UnityEditor.Editor
     {
+        static AddressableAssetGroup productGroup;
+        
+        [InitializeOnLoadMethod]
+        static void Init()
+        {
+            productGroup = AddressableAssetSettingsDefaultObject.Settings.FindGroup("Products");
+        }
+        
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
@@ -19,22 +29,50 @@ namespace Products.Editor
                 EditorGUILayout.HelpBox("Assign a product prefab.", MessageType.Error);
                 return;
             }
-            
-            if (prefab.TryGetComponent(out ProductIdentifier id) && id.productInfo == info) return;
 
-            EditorGUILayout.HelpBox(
-                $"The selected prefab does not have a {nameof(ProductIdentifier)} " +
-                $"or does not have the current {nameof(ProductInfo)} selected.",
-                MessageType.Warning);
-
-            if (GUILayout.Button("Fix Now"))
+            if (!prefab.TryGetComponent(out ProductIdentifier id) || id.productInfo != info)
             {
-                if (!id) id = prefab.AddComponent<ProductIdentifier>();
+                EditorGUILayout.HelpBox(
+                    $"The selected prefab does not have a {nameof(ProductIdentifier)} " +
+                    $"or does not have the current {nameof(ProductInfo)} selected.",
+                    MessageType.Warning);
 
-                id.productInfo = info;
-                
-                PrefabUtility.SavePrefabAsset(prefab);
+                if (GUILayout.Button("Fix Now"))
+                {
+                    if (!id) id = prefab.AddComponent<ProductIdentifier>();
+
+                    id.productInfo = info;
+
+                    PrefabUtility.SavePrefabAsset(prefab);
+                }
             }
+            
+            if (!IsAssetAddressable(info))
+            {
+                EditorGUILayout.HelpBox("This product is not properly set as an addressable.",
+                    MessageType.Error);
+
+                if (GUILayout.Button("Modify Addressable"))
+                    AddAssetToAddressables(info);
+            }
+        }
+        
+        static void AddAssetToAddressables(ProductInfo info)
+        {
+            var settings = AddressableAssetSettingsDefaultObject.Settings;
+            var assetPath = AssetDatabase.GetAssetPath(info);
+            var assetGuid = AssetDatabase.AssetPathToGUID(assetPath);
+            var entry = settings.CreateOrMoveEntry(assetGuid, productGroup);
+            entry.SetAddress(info.DisplayName);
+        }
+        
+        static bool IsAssetAddressable(ProductInfo info)
+        {
+            var settings = AddressableAssetSettingsDefaultObject.Settings;
+            var entry = settings.FindAssetEntry(AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(info)));
+            return entry != null
+                   && entry.parentGroup == productGroup
+                   && entry.address == info.DisplayName;
         }
     }
 }
