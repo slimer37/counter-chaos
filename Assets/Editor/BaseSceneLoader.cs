bc4e9a86ebf840d7a05c2330c8c16d9049b3d535
Alfred <59073967+slimer37@users.noreply.github.com>
2022-07-26 11:05:29 +0800

    Add menu toggle for base scene loader

--- Assets/Editor/BaseSceneLoader.cs
+++ Assets/Editor/BaseSceneLoader.cs
@@ -7,12 +7,33 @@ namespace Project.Editor
     public static class BaseSceneLoader
     {
         static readonly int[] ExcludedScenes = {0, 1};
+
+        const string MenuName = "Tools/Load Base Scene";
+        const string SettingName = "LoadBaseScene";
+        
+        static bool IsEnabled
+        {
+            get => EditorPrefs.GetBool(SettingName, true);
+            set => EditorPrefs.SetBool(SettingName, value);
+        }
+          
+        [MenuItem(MenuName)]
+        static void Toggle() => IsEnabled = !IsEnabled;
+
+        [MenuItem(MenuName, true)]
+        static bool ToggleValidate()
+        {
+            Menu.SetChecked(MenuName, IsEnabled);
+            return true;
+        }
         
         [InitializeOnLoadMethod]
         static void Init()
         {
             SceneManager.sceneLoaded += (scene, _) =>
             {
+                if (!IsEnabled) return;
+                
                 if (SceneManager.sceneCount == 1 && !ExcludedScenes.Contains(scene.buildIndex))
                     SceneManager.LoadScene("Base", LoadSceneMode.Additive);
             };
