d53775e8240ff232f7cea811abf6e1ed74a5974b
Alfred <59073967+slimer37@users.noreply.github.com>
2022-08-14 18:23:48 -0700

    Remove unused singleton type

--- Assets/Scripts/Core/Singleton.cs
+++ Assets/Scripts/Core/Singleton.cs
@@ -3,24 +3,6 @@ using UnityEngine;
 
 namespace Core
 {
-    public abstract class SelfStartSingleton<T> : MonoBehaviour where T : MonoBehaviour
-    {
-        static T instance;
-        
-        public static T Instance
-        {
-            get
-            {
-                if (instance) return instance;
-
-                instance = new GameObject(nameof(T)).AddComponent<T>();
-                DontDestroyOnLoad(instance.gameObject);
-                
-                return instance;
-            }
-        }
-    }
-
     public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         static T instance;
