54540126a50627013db46ff2fb54cd2bfb7c9bd1
Alfred <59073967+slimer37@users.noreply.github.com>
2022-09-05 17:31:47 -0700

    Add input field to sensitivity setting

--- Assets/Scripts/UI/Settings/PlayerModifiers/SensitivitySlider.cs
+++ Assets/Scripts/UI/Settings/PlayerModifiers/SensitivitySlider.cs
@@ -1,5 +1,5 @@
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace UI.Settings.PlayerModifiers
@@ -7,19 +7,39 @@ namespace UI.Settings.PlayerModifiers
     public class SensitivitySlider : MonoBehaviour
     {
         [SerializeField] Slider slider;
+        [SerializeField] TMP_InputField numInput;
         [SerializeField] PlayerController controller;
         
         void Awake()
         {
-            slider.value = PlayerPrefs.GetFloat(PlayerController.SensitivityPrefKey, PlayerController.DefaultSensitivity);
+            var value = PlayerPrefs.GetFloat(PlayerController.SensitivityPrefKey, PlayerController.DefaultSensitivity);
+            UpdateUI(value);
+            
             slider.onValueChanged.AddListener(ChangeSensitivity);
+
+            numInput.contentType = TMP_InputField.ContentType.DecimalNumber;
+            numInput.onSubmit.AddListener(ChangeSensitivity);
+            numInput.onDeselect.AddListener(ChangeSensitivity);
         }
+
+        void ChangeSensitivity(string value) => ChangeSensitivity(float.Parse(value));
 
         void ChangeSensitivity(float value)
         {
+            // Apply slider min and max by assigning it to the slider.
+            UpdateUI(value);
+            value = slider.value;
+            
             PlayerPrefs.SetFloat(PlayerController.SensitivityPrefKey, value);
+            
             if (!controller) return;
             controller.sensitivity = value;
+        }
+
+        void UpdateUI(float value)
+        {
+            slider.value = value;
+            numInput.text = slider.value.ToString("F1");
         }
     }
 }
